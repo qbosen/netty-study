bc488db159b38a1220917471662809fce3a4f762
abosen <abosen@qq.com>
2019-10-20 00:04:57 +0800

    protobuf 简单的使用

--- src/main/java/com/abosen/protobuf/package-info.java
+++ src/main/java/com/abosen/protobuf/package-info.java
@@ -1,6 +1,6 @@
 /**
  * DataInfo 是通过 $ protoc --java_out=src/main/java src/main/resources/protobuf/Student.proto 自动生成的Java类
- * protobuf/Student.proto 是
+ * protobuf/Student.proto 是按照google标准编写的描述文件
  * @author qiubaisen
  * @date 2019-10-20
  */
